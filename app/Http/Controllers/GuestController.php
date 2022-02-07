741165ce8bbe37848c01b96dcc868445991fb318
purpledev1337 <88691096+purpledev1337@users.noreply.github.com>
2022-02-07 11:12:40 +0100

    posts show in descendet creation date

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -16,7 +16,7 @@ class GuestController extends Controller
 
     public function posts()
     {
-        $posts = Post::all();
+        $posts = Post::orderBy('created_at', 'desc') -> get();
 
         return view('pages.posts', compact('posts'));
     }
